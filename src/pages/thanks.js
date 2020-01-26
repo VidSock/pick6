83d4d84848af51fa51862daeeece07044730e661
twilightscapes <ihatespam@gmail.com>
2020-01-26 05:14:10 -0600

    thanks page now links to home

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import Layout from '../components/Layout-noc'
+import logo from '../img/pick6golf-logo-square.svg'
+import { Link } from 'gatsby'
 
 const Terms = () => (
   <Layout>
     <section className="outer section section--gradient">
       <div className="container" style={{padding: '2rem'}}>
-      <h1>Thank you!</h1>
+      <h1 style={{textAlign:'center'}}>Thank you!</h1>
     
     <h3>You should receive an email confirming your picks, shortly.</h3>
 
-
-
+<div style={{display:'flex', justifyContent:'center', alignItems:'center',}}>
+<Link to="/" className="logolink" title="Logo - Back To Home">
+    <img style={{}} className="logo fadeInDown" src={logo} alt="Twilightscapes Logo" /><br />
+    Return to Home
+    </Link>
+</div>
 
 
     </div>
