b3897263d16734bc59411629f099f00379600353
twilightscapes <ihatespam@gmail.com>
2020-01-31 07:25:57 -0600

    Update thanks.js

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout-noc'
-import logo from '../img/pick6golf.svg'
+import logo from '../img/pick6golf-final.svg'
 import { Link } from 'gatsby'
 
 const Terms = () => (
@@ -11,9 +11,9 @@ const Terms = () => (
     
     <h3>You should receive an email confirming your picks, shortly.</h3>
 
-<div style={{display:'flex', justifyContent:'center', alignItems:'center',}}>
-<Link to="/" className="logolink" title="Logo - Back To Home">
-    <img style={{}} className="logo fadeInDown" src={logo} alt="Pick 6 Logo" /><br />
+<div style={{display:'flex', justifyContent:'center', alignItems:'center', textAlign:'center',}}>
+<Link to="/" className="" title="Logo - Back To Home">
+    <img style={{width:'400px',}} className="logo fadeInDown" src={logo} alt="Pick 6 Logo" /><br />
     Return to Home
     </Link>
 </div>
